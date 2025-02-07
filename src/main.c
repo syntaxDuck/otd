48ae1a2cee595903c9f77659752900fc7742dacb
syntaxDuck <kam.comer96@gmail.com>
2025-02-06 22:40:30 -0800

    Updated cmake file to include sodium for encryption, removed so test code from main

--- src/main.c
+++ src/main.c
@@ -1,87 +1,15 @@
 #include "generics.h"
 #include "hash_table.h"
+#include "sodium.h"
 #include <stdio.h>
 #include <string.h>
 
 int main() {
   HashTable *table = ht_create(DJB2, 0);
-
-  void *s = "Hello World!";
-  GenericValue value =
-      create_generic("Hello World", strlen(s), TYPE_CHAR_POINTER);
-  int i = 8;
-  GenericValue value1 = create_generic(&i, sizeof(int), TYPE_INT);
-  print_generic(value);
-  printf("\n");
-  print_generic(value1);
-  printf("\n");
-
-  ht_insert(table, "apple", "red");
-  ht_insert(table, "banana", "yellow");
-  ht_insert(table, "grape", "purple");
-  ht_insert(table, "orange", "orange");
-  ht_insert(table, "strawberry", "red");
-  ht_insert(table, "blueberry", "blue");
-  ht_insert(table, "cherry", "red");
-  ht_insert(table, "watermelon", "green");
-  ht_insert(table, "mango", "orange");
-  ht_insert(table, "peach", "pink");
-  ht_insert(table, "pear", "green");
-  ht_insert(table, "pineapple", "yellow");
-  ht_insert(table, "plum", "purple");
-  ht_insert(table, "kiwi", "brown");
-  ht_insert(table, "coconut", "brown");
-  ht_insert(table, "avocado", "green");
-  ht_insert(table, "lemon", "yellow");
-  ht_insert(table, "lime", "green");
-  ht_insert(table, "pomegranate", "red");
-  ht_insert(table, "cranberry", "red");
-  ht_insert(table, "fig", "purple");
-  ht_insert(table, "apricot", "orange");
-  ht_insert(table, "blackberry", "black");
-  ht_insert(table, "raspberry", "red");
-  // ht_insert(table, "papaya", "orange");
-  // ht_insert(table, "dragonfruit", "pink");
-  // ht_insert(table, "passionfruit", "purple");
-  // ht_insert(table, "cantaloupe", "orange");
-  // ht_insert(table, "honeydew", "green");
-  // ht_insert(table, "persimmon", "orange");
-  // ht_insert(table, "date", "brown");
-  // ht_insert(table, "olive", "green");
-  // ht_insert(table, "gooseberry", "green");
-  // ht_insert(table, "lychee", "red");
-  // ht_insert(table, "mulberry", "black");
-  // ht_insert(table, "tangerine", "orange");
-  // ht_insert(table, "boysenberry", "purple");
-  // ht_insert(table, "currant", "red");
-  // ht_insert(table, "elderberry", "black");
-  // ht_insert(table, "jackfruit", "yellow");
-  // ht_insert(table, "kumquat", "orange");
-  // ht_insert(table, "longan", "brown");
-  // ht_insert(table, "starfruit", "yellow");
-  // ht_insert(table, "soursop", "green");
-  // ht_insert(table, "quince", "yellow");
-  // ht_insert(table, "guava", "green");
-  // ht_insert(table, "cherimoya", "green");
-  // ht_insert(table, "sapodilla", "brown");
-  // ht_insert(table, "tamarind", "brown");
-  // ht_insert(table, "marionberry", "black");
-  // ht_insert(table, "ackee", "red");
-
-  // ht_print(table);
-
-  // ht_remove(table, "apple");
-  // ht_remove(table, "cranberry");
-  // ht_remove(table, "pineapple");
-  // ht_remove(table, "watermelon");
-
-  // ht_print(table);
-  // ht_resize(&table, 5);
-  // ht_print(table);
-  //
-  // ht_clear(table);
-  // ht_print(table);
-  free_ht(table);
-
+  if (sodium_init() < 0) {
+    perror("Error initializing sodium");
+    exit(1);
+  }
+  printf("We did it!");
   return 0;
 }
